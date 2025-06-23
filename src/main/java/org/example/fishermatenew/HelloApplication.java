a8f5b211a8da1fbc6821e211ae044cb076df863c
medhani premathilaka <mpremathilaka1@gmail.com>
2025-06-23 20:14:30 +0530

    Enhance application window style and properties for login interface

--- src/main/java/org/example/fishermatenew/HelloApplication.java
+++ src/main/java/org/example/fishermatenew/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,7 +13,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 300, 500);
-        stage.setTitle("FisherMate!");
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setTitle("FisherMate");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
