11c8a59dbb032fd1d550531fcbf1c1c26985189c
RCN <ravindunishshankawork@gmail.com>
2025-07-15 01:12:55 +0530

    corrected the error and now all the details can display

--- src/main/java/org/example/fishermatenew/models/RealMaxDays.java
+++ src/main/java/org/example/fishermatenew/models/RealMaxDays.java
@@ -1,6 +1,6 @@
 package org.example.fishermatenew.models;
 
-import org.example.fishermatenew.dao.DataRetriever;
+import org.example.fishermatenew.dao.DataRetrieverAndPrinter;
 import org.example.fishermatenew.dao.WeatherData;
 
 
@@ -11,7 +11,7 @@ public class RealMaxDays {
         int numberOfForecasts = inputData.getMaxDays() * FORECAST_CONSTANT;
         WeatherData data;
         for (int i = 0; i < numberOfForecasts; i++) {
-            data = DataRetriever.dataRetriever(inputData);
+            data = DataRetrieverAndPrinter.dataRetriever(inputData);
 
 
             assert data != null;
